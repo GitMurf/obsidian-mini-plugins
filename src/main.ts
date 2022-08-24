c9dd1c69bc0c4533c83e4570cfaec0d3e6edf16a
GitMurf <64155612+GitMurf@users.noreply.github.com>
2022-08-24 08:56:46 -0700

    Initial main.ts setup and cleanup removing sample stuff you do not need

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SampleModal } from './ui';
 
 export default class MyPlugin extends Plugin {
     settings: MyPluginSettings = DEFAULT_SETTINGS;
-    pluginName: string = 'Murf Sample Plugin Template';
+    pluginName: string = 'Obsidian Mini Plugins';
 
     async onload() {
         console.log(`Loading plugin: ${this.pluginName} at [${formatDate()}]`);
@@ -28,57 +28,15 @@ export default class MyPlugin extends Plugin {
 
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open sample modal (simple)',
+            id: `open-sample-modal-${this.pluginName.replace(/\s/g, '-')}`,
+            name: `Open sample modal for ${this.pluginName}`,
             callback: () => {
                 new SampleModal(this.app).open();
-            }
-        });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
-            editorCallback: (editor: Editor, view: MarkdownView) => {
-                console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
-            }
-        });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Open sample modal (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new SampleModal(this.app).open();
-                    }
-
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true;
-                }
             }
         });
 
         // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new SampleSettingTab(this.app, this));
-
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-            console.log('click', evt);
-        });
-
-        const min = 0;
-        const sec = 10;
-        const myInterval = window.setInterval(() => {
-            mySetIntervalFunction(this, myInterval);
-        }, (min * 60 * 1000) + (sec * 1000))
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        this.registerInterval(myInterval);
     }
 
     onunload() {
